ca53fb44530627946a9888e1ce7e97a0f21dca8a
Michał Kamiński <MICKAMI52@edu.gdansk.pl>
2022-09-11 00:54:15 +0200

    Version 1.1

---
+++ kursyNBP/DaneWaluty.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kursyNBP
+{
+    internal class DaneWaluty
+    {
+        public DateTime Data { get; set; }
+        public double Cena { get; set; }
+    }
+}
--- kursyNBP/MainWindow.xaml.cs
+++ kursyNBP/MainWindow.xaml.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Drawing.Printing;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
@@ -26,6 +28,8 @@ namespace kursyNBP
         public MainWindow()
         {
             InitializeComponent();
+            GetGoldPrice();
+            /*GetEuroPrice();*/
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -36,5 +40,49 @@ namespace kursyNBP
             // wyświetlam go jako okno dialogowe
             historiaWalut.ShowDialog();
         }
+
+        private void GetGoldPrice()
+        {
+            DaneWaluty waluta = new();
+            HttpClient clientZloto = new();
+
+            // Create new URL
+            clientZloto.BaseAddress = new Uri("http://api.nbp.pl/api/cenyzlota");
+            clientZloto.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            // List data response
+            HttpResponseMessage response = clientZloto.GetAsync(clientZloto.BaseAddress).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                // Write data from API to the list
+                var valueObject = response.Content.ReadAsAsync<IEnumerable<DaneWaluty>>().Result;
+                foreach (var data in valueObject)
+                {
+                    zloto.AppendText(data.Cena.ToString());
+                }
+            }
+        }
+
+      /*  private void GetEuroPrice()
+        {
+            DaneWaluty waluta = new();
+            HttpClient clientEuro = new();
+
+            // Create new URL
+            clientEuro.BaseAddress = new Uri("https://api.nbp.pl/api/exchangerates/rates/a/eur/");
+            clientEuro.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            // List data response
+            HttpResponseMessage response = clientEuro.GetAsync(clientEuro.BaseAddress).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                // Write data from API to the list
+                var valueObject = response.Content.ReadAsAsync<IEnumerable<DaneWaluty>>().Result;
+                foreach (var data in valueObject)
+                {
+                    euro.AppendText(data.Cena.ToString());
+                }
+            }
+        }*/
     }
 }
