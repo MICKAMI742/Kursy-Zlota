fcd4a30fe88c7aac912262351e50348b89be2bda
Michał Kamiński <MICKAMI52@edu.gdansk.pl>
2022-09-12 15:03:33 +0200

    Version 1.4

--- kursyNBP/HistoriaEuro.xaml.cs
+++ kursyNBP/HistoriaEuro.xaml.cs
@@ -24,6 +24,8 @@ namespace kursyNBP
         public HistoriaEuro()
         {
             InitializeComponent();
+            dataOd.SelectedDate = DateTime.Now;
+            dataDo.SelectedDate = DateTime.Now;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -32,31 +34,61 @@ namespace kursyNBP
         }
 
         private void GetEuroPriceHistory()
-        {
+        { 
             tabelaDanych.Items.Clear();
+            srednia.Document.Blocks.Clear();
+            mediana.Document.Blocks.Clear();
+            
             string? DataOd = dataOd.SelectedDate.Value.Date.ToString("yyyy-MM-dd");
             string? DataDo = dataDo.SelectedDate.Value.Date.ToString("yyyy-MM-dd");
+            
             HttpClient clientEuro = new();
 
             // Create new URL
             clientEuro.BaseAddress = new Uri($"https://api.nbp.pl/api/exchangerates/rates/c/eur/{DataOd}/{DataDo}");
             clientEuro.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
             HttpResponseMessage response = clientEuro.GetAsync(clientEuro.BaseAddress).Result;
+            
             if (response.IsSuccessStatusCode)
             {
                 var valueObject = response.Content.ReadAsAsync<TablicaDanychEuro>().Result;
                 foreach(var data in valueObject.rates)
                 {
                     tabelaDanych.Items.Add(data);
+                    
                 }
 
                 double? suma = 0;
+                double? price = 0;
+                List<double?> daneEuro = new List<double?>();
 
-                foreach(var data in valueObject.rates)
+                foreach (var data in valueObject.rates)
                 {
                     suma += data.ask;
+                    price = data.ask;
+                    daneEuro.Add(price);
                 }
+
+                daneEuro.Sort();
+                daneEuro.Count();
                 srednia.AppendText((suma / valueObject.rates.Length).ToString());
+                
+                if (valueObject.rates.Length % 2 == 0)
+                {
+                    double? medianaParzysta = (daneEuro[(valueObject.rates.Length / 2) - 1] + daneEuro[(valueObject.rates.Length / 2)])/2;
+                    mediana.AppendText(medianaParzysta.ToString());
+                }
+                else if (valueObject.rates.Count() == 1)
+                {
+                    foreach (var data in valueObject.rates)
+                    {
+                        mediana.AppendText(data.ask.ToString());
+                    }
+                }
+                else
+                {
+                    mediana.AppendText(daneEuro[(valueObject.rates.Length / 2) - 1].ToString());
+                }
             }
         }
     }
--- kursyNBP/HistoriaZloto.xaml.cs
+++ kursyNBP/HistoriaZloto.xaml.cs
@@ -25,6 +25,8 @@ namespace kursyNBP
         public HistoriaZloto()
         {
             InitializeComponent();
+            dataOd.SelectedDate = DateTime.Now;
+            dataDo.SelectedDate = DateTime.Now;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -35,14 +37,19 @@ namespace kursyNBP
         private void GetGoldPriceHistory()
         {
             tabelaDanych.Items.Clear();
+            srednia.Document.Blocks.Clear();
+            mediana.Document.Blocks.Clear();
+
             string? DataOd = dataOd.SelectedDate.Value.Date.ToString("yyyy-MM-dd");
             string? DataDo = dataDo.SelectedDate.Value.Date.ToString("yyyy-MM-dd");
+
             HttpClient clientEuro = new();
 
             // Create new URL
             clientEuro.BaseAddress = new Uri($"http://api.nbp.pl/api/cenyzlota/{DataOd}/{DataDo}");
             clientEuro.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
             HttpResponseMessage response = clientEuro.GetAsync(clientEuro.BaseAddress).Result;
+
             if (response.IsSuccessStatusCode)
             {
                 var valueObject = response.Content.ReadAsAsync<IEnumerable<DaneWaluty>>().Result;
@@ -50,6 +57,35 @@ namespace kursyNBP
                 {
                     tabelaDanych.Items.Add(data);
                 }
+
+                List<double?> daneZlota = new List<double?>();
+                double? suma = 0;
+
+                foreach(var data in valueObject)
+                {
+                    suma += data.Cena;
+                    daneZlota.Add(data.Cena);
+                }
+
+                daneZlota.Sort();
+
+                if (valueObject.Count() % 2 == 0)
+                {
+                    double? medianaParzysta = (daneZlota[(valueObject.Count() / 2) - 1] + daneZlota[(valueObject.Count() / 2)]) / 2;
+                    mediana.AppendText(medianaParzysta.ToString());
+                }else if (valueObject.Count() == 1)
+                {
+                    foreach (var data in valueObject)
+                    {
+                        mediana.AppendText(data.Cena.ToString());
+                    }
+                }
+                else
+                {
+                    mediana.AppendText(daneZlota[(valueObject.Count() / 2) - 1].ToString());
+                }
+
+                srednia.AppendText((suma / valueObject.Count()).ToString());
             }
         }
     }
